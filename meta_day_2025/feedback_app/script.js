f4b9e42de9d3417b91d64f372c59dc2428fc87c7
thaisnakazone <thais.nakazone@alumni.usp.br>
2025-11-07 16:00:43 -0300

    update

--- meta_day_2025/feedback_app/script.js
+++ meta_day_2025/feedback_app/script.js
@@ -19,13 +19,13 @@ import {
 // 2. CONFIG FIREBASE
 // ===============================
 const firebaseConfig = {
-  apiKey: "AIzaSyBinNESW5Am9fV-5EH4hEfBvhxgqwfRwfE",
-  authDomain: "metaday-fatecsebrae.firebaseapp.com",
-  projectId: "metaday-fatecsebrae",
-  storageBucket: "metaday-fatecsebrae.firebasestorage.app",
-  messagingSenderId: "813356147967",
-  appId: "1:813356147967:web:40ec819dee718dd4e0d8ca",
-  measurementId: "G-8Y56FGSNVL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const db = getFirestore(app);
