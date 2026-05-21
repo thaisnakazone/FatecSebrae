cc1850fc08c734691f704c6e569d9c87f455c65c
thaisnakazone <thais.nakazone@alumni.usp.br>
2026-05-21 13:32:23 -0300

    Subindo arquitetura completa (MVC + MVP)

---
+++ 2026/architecture_patterns/mvp/view.py
@@ -0,0 +1,15 @@
+class SuspeitoView:
+    # A View agora apenas recebe ordens do que imprimir
+    def exibir_sucesso(self, mensagem):
+        print(f"\n[OK] {mensagem}")
+
+    def exibir_erro(self, mensagem):
+        print(f"\n[ERRO] {mensagem}")
+
+    def mostrar_tabela_suspeitos(self, lista_formatada):
+        print("\n--- RELATÓRIO DE INVESTIGAÇÃO ---")
+        for linha in lista_formatada:
+            print(linha)
+
+    def obter_input_usuario(self, prompt):
+        return input(prompt)
